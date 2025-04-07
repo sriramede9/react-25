15b2ce14adf19e1af3f5d9b704da8d30ed0a40a9
sriramede9 <only4programming03@gmail.com>
2025-04-07 17:53:35 -0400

    simple and elegant rating

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 // import Accordion from './components/accordian';
 // import data from './components/accordian/data';
 // import RandomColor from './components/random-color';
-import RateStars from './components/rate-stars';
+// import RateStars from './components/rate-stars';
+import RateStars2 from './components/rate-stars/index2';
 
 function App() {
   return (
     <div className="App">
       {/* <Accordion data={data}/> */}
       {/* <RandomColor /> */}
-      <RateStars />
+      <RateStars2 />
     </div>
   );
 }
