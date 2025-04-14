031551ab32fe47d3119bf0af27de08f202c0bec1
sriramede9 <only4programming03@gmail.com>
2025-04-14 19:05:32 -0400

    load more data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 // import Accordion from './components/accordian';
-import data from './components/image-slider/data';
-import ImageSlider from './components/image-slider';
+import LoadMore from './components/load-more';
+
 
 function App() {
   return (
     <div className="App">
-      <ImageSlider data={data}/>
+      {/* <ImageSlider data={data}/> */}
+      {/* <Accordion /> */}
+      <LoadMore />
     </div>
   );
 }
---
+++ src/components/load-more/index.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import './styles.css';
+
+const LoadMore = () => {
+  const [products, setProducts] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const [count, setCount] = useState(0);
+
+  async function fetchProducts() {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
+
+      const result = await response.json();
+        setLoading(false);
+        if(result && result.products && result.products.length > 0) {
+          setProducts(result.products);
+        }
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, [count]);
+
+
+  return (
+    <div className="container"> 
+      <h1>Load More</h1>
+      {loading && <p>Loading...</p>}
+      {products && products.length > 0 ? (
+        <div className="product-container">
+          {products.map((product) => (
+            <div key={product.id} className="product">
+              <img src={product.thumbnail} alt={product.name} />
+              <h2>{product.name}</h2>
+              <p>{product.description}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>No products available</p>
+      )}
+      {!loading && (
+        <button onClick={() => setCount(count + 1)}>Load More</button>
+      )}
+    </div>
+  );
+};
+
+export default LoadMore;
